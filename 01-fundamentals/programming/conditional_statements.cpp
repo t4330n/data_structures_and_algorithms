3ae75337e9e1389f089c5d3c7f2846a867954a17
t4330n <t4330n@gmail.com>
2026-08-26 00:16:11 +0900

    feat: conditional_statements

---
+++ 01-fundamentals/programming/conditional_statements.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+// // If Conditional Statement
+// int main() {
+//   int x = 7;
+
+//   if (x > 0) {
+//     cout << "x is positive";
+//   }
+
+//   return 0;
+// }
+
+// // If-Else Conditional Statement
+// int main() {
+//   int x = 7;
+
+//   if (x > 0) {
+//     cout << "x is positive";
+//   } else {
+//     cout << "x is not positive";
+//   }
+
+//   return 0;
+// }
+
+// // if-Else if Conditional Statement
+// int main() {
+//   int x = 7;
+
+//   if (x > 0) {
+//     cout << "x is positive";
+//   } else {
+//     cout << "x is not positive";
+//   }
+
+//   return 0;
+// }
+
+// // Switch Conditional Statement
+// int main() {
+//   int day = 2;
+
+//   switch (day) {
+//     case 1:
+//       cout << "Monday" << endl;
+//       break;
+//     case 2:
+//       cout << "Tuesday" << endl;
+//       break;
+//     case 3:
+//       cout << "Wednesday" << endl;
+//       break;
+
+//     default:
+//       cout << "Invalid day!" << endl;
+//   }
+
+//   return 0;
+// }
+
+// // Ternary Expression Conditional Statement
+// int main() {
+//   int x = 10;
+//   cout << (x > 0 ? "x is positive" : "x is not positive");
+
+//   return 0;
+// }
