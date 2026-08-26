1aaa7a629099cd5bd694a0547e191cb29048c0e2
t4330n <t4330n@gmail.com>
2026-08-26 11:21:26 +0900

    feat: function

---
+++ 01-fundamentals/programming/function.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+
+// Built-in functions
+int main() {
+  double result = sqrt(25.0);
+  cout << "Square Root: " << result << endl;
+
+  return 0;
+}
+
+// User-defined
+void greet() { cout << "Hello, World!" << endl; }
+
+int main() {
+  greet();
+
+  return 0;
+}
