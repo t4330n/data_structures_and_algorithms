883eb011d1561a7c44d8693739758e5d4a6de4f8
t4330n <t4330n@gmail.com>
2026-08-26 09:06:47 +0900

    feat: for_loop

---
+++ 01-fundamentals/programming/for_loop.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+// // Basic For Loop
+// int main() {
+//   for (int i = 2; i <= 10; i += 2) {
+//     cout << i << " ";
+//   }
+// }
+
+// // For Each Loop
+// int main() {
+//   int numbers[] = {1, 2, 3, 4, 5};
+//   for (int num : numbers) {
+//     cout << num << " ";
+//   }
+// }
+
+// // For Loop with Multiple Variables
+// int main() {
+//   for (int i = 0, j = 10; i < 5 && j > 0; i++, j--) {
+//     cout << "i=" << i << ", j=" << j << endl;
+//   }
+// }
+
+// // Infinite For Loop
+// int main() {
+//   for (;;) {
+//     // Infinite loop
+//   }
+// }
+
+// // Nested For Loop
+// int main() {
+//   for (int i = 1; i <= 3; i++) {
+//     for (int j = 1; j <= 3; j++) {
+//       cout << i * j << " ";
+//     }
+//     cout << endl;
+//   }
+
+//   return 0;
+// }
+
+// // For Loop with Step/Stride
+// int main() {
+//   for (int i = 0; i < 10; i += 2) {
+//     cout << i << " ";
+//   }
+// }
