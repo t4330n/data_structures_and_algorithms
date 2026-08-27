17737894b1217f42a53fc5550a53f335b35a8809
t4330n <t4330n@gmail.com>
2026-08-27 14:29:27 +0900

    feat: even_or_odd

---
+++ 02-maths_pattern_recursion/easy_maths/even_or_odd.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+// // [Naive Approach] By Finding the Remainder - O(1) Time and O(1) Space
+// bool isEven(int n) {
+//   int rem = n % 2;
+//   if (rem == 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// int main() {
+//   int n = 15;
+//   if (isEven(n))
+//     cout << "true";
+//   else
+//     cout << "false";
+
+//   return 0;
+// }
+
+// // [Efficient Approach] Using Bitwise AND Operator - O(1) Time and O(1) Space
+// bool isEven(int n) {
+//   if ((n & 1) == 0)
+//     return true;
+//   else
+//     return false;
+// }
+
+// int main() {
+//   int n = 15;
+//   if (isEven(n) == true)
+//     cout << "true";
+//   else
+//     cout << "false";
+
+//   return 0;
+// }
