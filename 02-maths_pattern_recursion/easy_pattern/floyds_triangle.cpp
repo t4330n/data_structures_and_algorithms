d4602d6a36f42acfa583b6bc1135a8e0ff9010fe
t4330n <t4330n@gmail.com>
2026-08-29 22:21:45 +0900

    feat: floyds_triangle

---
+++ 02-maths_pattern_recursion/easy_pattern/floyds_triangle.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// Using Nested Loops - O(n²) Time and O(1) Space
+void printfloydtriangle(int n) {
+  int val = 1;
+  for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= i; j++) {
+      cout << val++ << " ";
+    }
+    cout << endl;
+  }
+}
+
+int main() {
+  printfloydtriangle(6);
+  return 0;
+}
