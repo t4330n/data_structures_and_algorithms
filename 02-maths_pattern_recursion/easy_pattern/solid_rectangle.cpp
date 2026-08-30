f5c24c803b88a2dd0f19f29974374a36e15ce115
t4330n <t4330n@gmail.com>
2026-08-30 23:02:31 +0900

    refacor: solid_rectangle

--- 02-maths_pattern_recursion/easy_pattern/solid_rectangle.cpp
+++ 02-maths_pattern_recursion/easy_pattern/solid_rectangle.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-// Using Nested Loops – O(n*m) Time and O(1) Space
+// Using Nested Loops
+// O(n*m) Time and O(1) Space
 int main() {
   int n = 3, m = 5;
 
@@ -9,10 +10,7 @@ int main() {
     for (int j = 1; j <= m; j++) {
       cout << "* ";
     }
-
-    // Move to the next row
     cout << "\n";
   }
-
   return 0;
 }
