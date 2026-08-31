35340cccf59aded3fa83ed11e407557335375100
t4330n <t4330n@gmail.com>
2026-09-01 00:39:34 +0900

    theory: array

---
+++ 03-array_string/theory/array.cpp
@@ -0,0 +1,9 @@
+// Declaration of Array
+int arr[5];
+char arr[10];
+float arr[20];
+
+// Initialization of Array
+int arr[] = {1, 2, 3, 4, 5};
+char arr[5] = {'a', 'b', 'c', 'd', 'e'};
+float arr[10] = {1.4, 2.0, 24, 5.0, 0.0};
